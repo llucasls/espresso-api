6874ce7176a5d36ee5507933a86417370af42039
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-05-11 08:56:54 -0300

    chore: create new error messages
    The helper scripts can't be called directly from the command line.
    Instead, they should be executed using yarn. This commit adds error
    messages in case the user tries to execute them from the shell.

--- scripts/test-mocha.sh
+++ scripts/test-mocha.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/sh
+
+pid="$(pgrep $(basename $0))"
+gpprocess="$(ps -o comm= -p $(ps -o ppid= -p $(ps -o ppid= -p $pid)))"
+if [ "$gpprocess" != "node" ]; then
+    echo 'error: cannot execute script\n' > /dev/stderr
+    echo 'In order to run this script, you should rather use the following command:' > /dev/stderr
+    echo '\033[32myarn test\033[00m' > /dev/stderr
+    exit 10
+fi
 
 if [ -n "$1" ]; then
     mocha -g "$@"
