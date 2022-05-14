80df58d4422c4b504978639c6bcbcec2516e9afa
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-05-14 15:27:26 -0300

    chore: check both parent and grandparent process

--- scripts/manage-db.sh
+++ scripts/manage-db.sh
@@ -3,7 +3,8 @@
 pid="$(pgrep $(basename $0))"
 pprocess="$(ps -o comm= -p $(ps -o ppid= -p $pid))"
 gpprocess="$(ps -o comm= -p $(ps -o ppid= -p $(ps -o ppid= -p $pid)))"
-if [ "$gpprocess" != "node" ]; then
+
+if test "$pprocess" != "node" && "$gpprocess" != "node"; then
     echo 'error: cannot execute script\n' > /dev/stderr
     echo 'This script is meant to automate database tasks' > /dev/stderr
     echo 'It is not for using directly from the shell' > /dev/stderr
--- scripts/test-mocha.sh
+++ scripts/test-mocha.sh
@@ -3,7 +3,8 @@
 pid="$(pgrep $(basename $0))"
 pprocess="$(ps -o comm= -p $(ps -o ppid= -p $pid))"
 gpprocess="$(ps -o comm= -p $(ps -o ppid= -p $(ps -o ppid= -p $pid)))"
-if [ "$gpprocess" != "node" ]; then
+
+if test "$pprocess" != "node" && "$gpprocess" != "node"; then
     echo 'error: cannot execute script\n' > /dev/stderr
     echo 'In order to run this script, you should rather use the following command:' > /dev/stderr
     echo '\033[32myarn test\033[00m' > /dev/stderr
